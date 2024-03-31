467ccecafa67c19dd7e744b8cad351e2d43a7d32
Dave Best <dave.e.best@gmail.com>
2024-03-30 20:42:03 -0400

    adding the survey

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,17 @@ import { NavLink } from 'react-router-dom';
 function Header() {
     return (
         <header className="bg-blue-500 text-white p-6">
-            <img src="logo-placeholder.png" alt="Logo" className="h-12 w-auto" />
             <h1 className="text-4xl">Logan County Recovery</h1>
             <nav>
-                <NavLink to="/" exact className="text-white mx-2 hover:text-yellow-300" activeClassName="underline">Home</NavLink>
-                <NavLink to="/want-to-help" className="text-white mx-2 hover:text-yellow-300" activeClassName="underline">Want to Help</NavLink>
-                <NavLink to="/need-help" className="text-white mx-2 hover:text-yellow-300" activeClassName="underline">Need Help</NavLink>
+                <NavLink to="/" end className={({ isActive }) =>
+                    isActive ? 'text-white mx-4 hover:text-yellow-300 underline' : 'text-white mx-2 hover:text-yellow-300'
+                }>Home</NavLink>
+                <NavLink to="/want-to-help" className={({ isActive }) =>
+                    isActive ? 'text-white mx-4 hover:text-yellow-300 underline' : 'text-white mx-2 hover:text-yellow-300'
+                }>Volunteers</NavLink>
+                <NavLink to="/need-help" className={({ isActive }) =>
+                    isActive ? 'text-white mx-4 hover:text-yellow-300 underline' : 'text-white mx-2 hover:text-yellow-300'
+                }>Need Help</NavLink>
             </nav>
         </header>
     );
