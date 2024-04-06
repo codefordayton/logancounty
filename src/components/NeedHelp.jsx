016b24f2dbcbe3b586ac92608fd49b8b43d61b65
Dave Best <dave.e.best@gmail.com>
2024-04-06 13:09:02 -0400

    Cleaning up the formatting of the results.

--- src/components/NeedHelp.jsx
+++ src/components/NeedHelp.jsx
@@ -1,24 +1,24 @@
-import React, { useState, useCallback} from "react";
+import React, { useState, useCallback } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import { Model } from 'survey-core';
-import { Survey } from 'survey-react-ui';
-import survey from '../data/survey.json'
+import { Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+import survey from "../data/survey.json";
 
-import 'survey-core/defaultV2.min.css';
+import "survey-core/defaultV2.min.css";
 
 const _SurveyComp = ({ setIsCompleted }) => {
-    const surveyConfig = new Model(survey);
-    surveyConfig.focusFirstQuestionAutomatic = false;
-  
-    const alertResults = useCallback((sender) => {
-      setIsCompleted(true);
-    }, []);
-  
-    surveyConfig.onComplete.add(alertResults);
-  
-    return <Survey model={surveyConfig} />;
-  }
+  const surveyConfig = new Model(survey);
+  surveyConfig.focusFirstQuestionAutomatic = false;
+
+  const alertResults = useCallback((sender) => {
+    setIsCompleted(true);
+  }, []);
+
+  surveyConfig.onComplete.add(alertResults);
+
+  return <Survey model={surveyConfig} />;
+};
 
 const SurveyComp = React.memo(_SurveyComp);
 
@@ -34,7 +34,14 @@ function NeedHelp() {
       <Header />
       <main className="p-6">
         <SurveyComp setIsCompleted={setIsCompleted} />
-        {isCompleted && <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => refreshPage()}>Restart</button>}
+        {isCompleted && (
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => refreshPage()}
+          >
+            Restart
+          </button>
+        )}
       </main>
       <Footer />
     </div>
